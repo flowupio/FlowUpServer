76fecbdf4d09a2662349052b8d220ccad05fb88d
Davide Mendolia <davide@gokarumi.com>
2016-10-04 11:57:29 +0200

    Updated to version 2.5

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,12 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
-    public static Result index() {
+    public Result index() {
         return ok(index.render("Your new application is ready."));
     }
 
