41e725849efd54d928dc01f357679e0d7c09e5ca
Davide Mendolia <davide@gokarumi.com>
2016-10-06 18:54:51 +0200

    Add Format of ReportRequest

--- app/controllers/ReportController.java
+++ app/controllers/ReportController.java
@@ -8,6 +8,8 @@ import play.mvc.Result;
 import usecases.InsertDataPoints;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 public class ReportController extends Controller {
@@ -29,7 +31,58 @@ public class ReportController extends Controller {
 }
 
 class ReportRequest {
+    public String appPackage;
+    public String deviceModel;
+    public String screenDensity;
+    public String screenSize;
+    public String installationUUID;
+    public int numberOfCores;
+    public List<Network> network = new ArrayList<Network>();
+    public List<Ui> ui = new ArrayList<Ui>();
+    public List<Cpu> cpu = new ArrayList<Cpu>();
+    public List<Gpu> gpu = new ArrayList<Gpu>();
+
+    static class Network {
+        public int timestamp;
+        public String versionName;
+        public String androidOSVersion;
+        public boolean baterySaverOn;
+        public int bytesUploaded;
+        public int bytesDownloaded;
+    }
+
+    static class Ui {
+        public int timestamp;
+        public String versionName;
+        public String androidOSVersion;
+        public boolean baterySaverOn;
+        public String screenName;
+        public int frameTime;
+        public StatisticalValue fps;
+    }
+
+    static class Cpu {
+        public int timestamp;
+        public String versionName;
+        public String androidOSVersion;
+        public boolean baterySaverOn;
+        public int consumption;
+    }
+
+    static class Gpu {
+        public int timestamp;
+        public String versionName;
+        public String androidOSVersion;
+        public boolean baterySaverOn;
+        public int consumption;
+    }
+
+    static class StatisticalValue {
+        public int avg;
+        public int p90;
+    }
 }
+
 
 class ReportResponse {
     public String message;
