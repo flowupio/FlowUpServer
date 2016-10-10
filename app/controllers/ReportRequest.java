f6596dd6c3b2868cddac3723250498f8e49e3233
Davide Mendolia <davide@gokarumi.com>
2016-10-10 10:59:48 +0200

    Update Parsing format

--- app/controllers/ReportRequest.java
+++ app/controllers/ReportRequest.java
@@ -1,57 +1,86 @@
 package controllers;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 class ReportRequest {
-    public String appPackage;
-    public String deviceModel;
-    public String screenDensity;
-    public String screenSize;
-    public String installationUUID;
-    public int numberOfCores;
-    public List<Network> network = new ArrayList<Network>();
-    public List<Ui> ui = new ArrayList<Ui>();
-    public List<Cpu> cpu = new ArrayList<Cpu>();
-    public List<Gpu> gpu = new ArrayList<Gpu>();
+    private final String appPackage;
+    private final String deviceModel;
+    private final String screenDensity;
+    private final String screenSize;
+    private final String installationUUID;
+    private final double numberOfCores;
+    private final List<Network> network = new ArrayList<Network>();
+    private final List<Ui> ui = new ArrayList<Ui>();
+    private final List<Cpu> cpu = new ArrayList<Cpu>();
+    private final List<Gpu> gpu = new ArrayList<Gpu>();
 
+    @Data
     static class Network {
-        public int timestamp;
-        public String versionName;
-        public String androidOSVersion;
-        public boolean baterySaverOn;
-        public int bytesUploaded;
-        public int bytesDownloaded;
+        private final long timestamp;
+        private final String versionName;
+        private final String androidOSVersion;
+        private final boolean baterySaverOn;
+        private final double bytesUploaded;
+        private final double bytesDownloaded;
     }
 
+    @Data
     static class Ui {
-        public int timestamp;
-        public String versionName;
-        public String androidOSVersion;
-        public boolean baterySaverOn;
-        public String screenName;
-        public int frameTime;
-        public StatisticalValue fps;
+        private final long timestamp;
+        private final String versionName;
+        private final String androidOSVersion;
+        private final boolean baterySaverOn;
+        private final String screenName;
+        private final StatisticalValue frameTime;
+        private final StatisticalValue framesPerSecond;
     }
 
+    @Data
     static class Cpu {
-        public int timestamp;
-        public String versionName;
-        public String androidOSVersion;
-        public boolean baterySaverOn;
-        public int consumption;
+        private final long timestamp;
+        private final String versionName;
+        private final String androidOSVersion;
+        private final boolean baterySaverOn;
+        private final double consumption;
     }
 
+    @Data
     static class Gpu {
-        public int timestamp;
-        public String versionName;
-        public String androidOSVersion;
-        public boolean baterySaverOn;
-        public int consumption;
+        private final long timestamp;
+        private final String versionName;
+        private final String androidOSVersion;
+        private final boolean baterySaverOn;
+        private final double consumption;
     }
 
+    @Data
     static class StatisticalValue {
-        public int avg;
-        public int p90;
+        private final long count;
+        private final double min;
+        private final double max;
+        private final double mean;
+        private final double standardDev;
+        private final double median;
+        private final double p5;
+        private final double p10;
+        private final double p15;
+        private final double p20;
+        private final double p25;
+        private final double p30;
+        private final double p40;
+        private final double p50;
+        private final double p60;
+        private final double p70;
+        private final double p75;
+        private final double p80;
+        private final double p85;
+        private final double p90;
+        private final double p95;
+        private final double p98;
+        private final double p99;
     }
 }
--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ lazy val `flowupserver` = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq( javaJdbc ,  cache , javaWs,
+  "org.projectlombok" % "lombok" % "1.16.10",
   "com.novocode" % "junit-interface" % "0.11" % "test",
   "org.mockito" % "mockito-all" % "1.10.19" % "test",
   "org.hamcrest" % "hamcrest-junit" % "2.0.0.0" % "test"
