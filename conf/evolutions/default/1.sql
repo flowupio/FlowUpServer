42f4a4b46704d164f96284108ea4d697d806730f
Davide Mendolia <davide@gokarumi.com>
2016-10-13 11:21:47 +0200

    Use mysql and setup for config in AWS with ENV vars

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,11 +4,11 @@
 # --- !Ups
 
 create table user (
-  id                            uuid not null,
+  id                            varchar(40) not null,
   email                         varchar(255),
   name                          varchar(255),
-  active                        boolean,
-  email_validated               boolean,
+  active                        tinyint(1) default 0,
+  email_validated               tinyint(1) default 0,
   constraint uq_user_email unique (email),
   constraint pk_user primary key (id)
 );
