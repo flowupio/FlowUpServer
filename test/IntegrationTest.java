11212eba872b4f2cac0d7ec5e8acacd6a84b9703
Davide Mendolia <davide@gokarumi.com>
2016-10-17 13:41:58 +0200

    Add login test

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -3,6 +3,11 @@ import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.test.WithBrowser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -10,7 +15,6 @@ public class IntegrationTest extends WithBrowser {
 
     @Override
     protected Application provideApplication() {
-
         return new GuiceApplicationBuilder()
                 .build();
     }
@@ -19,5 +23,15 @@ public class IntegrationTest extends WithBrowser {
     public void testIndex() {
         browser.goTo("/");
         assertTrue(browser.pageSource().contains("Welcome to FlowUp"));
+    }
+
+    @Test
+    public void testLogin() {
+        browser.goTo("/login");
+        assertNotNull(browser.$("div#logins a"));
+        List<String> logins = new ArrayList<>();
+        logins.add("github");
+        logins.add("google");
+        assertEquals(logins, (browser.$("div#logins a").getTexts()));
     }
 }
