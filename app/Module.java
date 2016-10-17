c34673d5f1e3f511d94db4ee28c3b5c255d24f13
Davide Mendolia <davide@gokarumi.com>
2016-10-17 17:23:51 +0200

    Extract to module.AuthenticationModule

--- app/Module.java
+++ app/Module.java
@@ -1,13 +1,8 @@
-import com.feth.play.module.pa.Resolver;
-import com.feth.play.module.pa.providers.oauth2.github.GithubAuthProvider;
-import com.feth.play.module.pa.providers.oauth2.google.GoogleAuthProvider;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import datasources.ElasticSearchDatasource;
 import play.Configuration;
 import play.Environment;
-import service.AuthenticationResolver;
-import service.FlowUpUserService;
 import usecases.MetricsDatasource;
 
 public class Module extends AbstractModule {
@@ -32,13 +27,5 @@ public class Module extends AbstractModule {
         bind(MetricsDatasource.class)
                 .to(ElasticSearchDatasource.class)
                 .asEagerSingleton();
-
-
-        // play-authenticate dependencies
-        bind(Resolver.class).to(AuthenticationResolver.class);
-        // Following class depend on PlayAuthenticate auth, and they self register to it.
-        bind(GithubAuthProvider.class).asEagerSingleton();
-        bind(GoogleAuthProvider.class).asEagerSingleton();
-        bind(FlowUpUserService.class).asEagerSingleton();
     }
 }
