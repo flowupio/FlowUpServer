4d5c061f3cffcd1f9b1834275c6b4799dc21aed5
Davide Mendolia <davide@gokarumi.com>
2016-10-17 15:39:52 +0200

    Remove usage of UserService

--- app/controllers/CommandCenterController.java
+++ app/controllers/CommandCenterController.java
@@ -14,16 +14,14 @@ public class CommandCenterController extends Controller {
 
     private final PlayAuthenticate auth;
 
-    private final UserService userService;
 
     @Inject
-    public CommandCenterController(final PlayAuthenticate auth, final UserService userService) {
+    public CommandCenterController(final PlayAuthenticate auth) {
         this.auth = auth;
-        this.userService = userService;
     }
 
     public Result index() {
-        final User localUser = this.userService.getLocalUser(this.auth.getUser(session()));
+        final User localUser =  User.findByAuthUserIdentity(this.auth.getUser(session()));
         return ok(commandcenter.render(this.auth, localUser));
     }
 }
