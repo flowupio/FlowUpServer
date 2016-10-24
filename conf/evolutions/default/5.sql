c4b3d57318cbffb3490e232d3b77e2826455ca9d
Davide Mendolia <davide@gokarumi.com>
2016-10-24 19:40:38 +0200

    Fix SQL for test

--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -1,6 +1,7 @@
 # --- !Ups
 
-ALTER TABLE organization ADD grafana_id varchar(255), ADD google_account varchar(255);
+ALTER TABLE organization ADD grafana_id varchar(255);
+ALTER TABLE organization ADD google_account varchar(255);
 
 # --- !Downs
 
