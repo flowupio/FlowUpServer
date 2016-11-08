6656bb0f1a77d9f2c4325e1432a8fd6154d45f84
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2016-11-08 20:26:14 +0100

    Change constant to private instead of public

--- app/repositories/ApiKeyRepository.java
+++ app/repositories/ApiKeyRepository.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class ApiKeyRepository {
 
     private static final int API_KEY_CACHE_TTL = (int) TimeUnit.HOURS.toSeconds(1);
-    public static final String API_KEY_CACHE_KEY = "apiKey.value.";
+    private static final String API_KEY_CACHE_KEY = "apiKey.value.";
 
     private CacheApi cache;
     private ApiKeyDatasource apiKeyDatasource;
