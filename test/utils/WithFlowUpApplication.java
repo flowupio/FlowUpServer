10b1eb8de770e31e4a41e3a8e916a22dd890516d
Davide Mendolia <davide@gokarumi.com>
2016-11-10 00:45:16 +0100

    Use truncate tables instead of drop tables

--- test/utils/WithFlowUpApplication.java
+++ test/utils/WithFlowUpApplication.java
@@ -1,9 +1,12 @@
 package utils;
 
 import org.junit.After;
+import play.Logger;
 import play.db.Database;
-import play.db.evolutions.Evolutions;
 import play.test.WithApplication;
+
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class WithFlowUpApplication extends WithApplication {
 
@@ -16,8 +19,23 @@ public class WithFlowUpApplication extends WithApplication {
 
     private void cleanDatabase() {
         Database database = app.injector().instanceOf(Database.class);
-        Evolutions.cleanupEvolutions(database);
-        //		Ebean.createSqlUpdate("truncate users cascade").execute()
-
+        try {
+            Statement statement = database.getConnection().createStatement();
+            statement.addBatch("SET FOREIGN_KEY_CHECKS = 0;");
+            statement.addBatch("TRUNCATE api_key;");
+            statement.addBatch("TRUNCATE application;");
+            statement.addBatch("TRUNCATE linked_account;");
+            statement.addBatch("TRUNCATE organization;");
+            statement.addBatch("TRUNCATE organization_user;");
+            statement.addBatch("TRUNCATE security_role;");
+            statement.addBatch("TRUNCATE user;");
+            statement.addBatch("TRUNCATE user_permission;");
+            statement.addBatch("TRUNCATE user_security_role;");
+            statement.addBatch("TRUNCATE user_user_permission;");
+            statement.addBatch("SET FOREIGN_KEY_CHECKS = 1;");
+            statement.executeBatch();
+        } catch (SQLException e) {
+            Logger.error(e.getMessage());
+        }
     }
 }
