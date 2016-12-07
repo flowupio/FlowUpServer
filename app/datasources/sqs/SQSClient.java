434039456dcd78c36f36f0a79073ad6b97eb691d
Davide Mendolia <davide@gokarumi.com>
2016-12-07 18:21:34 +0000

    Add processing in sqsclient

--- app/datasources/sqs/SQSClient.java
+++ app/datasources/sqs/SQSClient.java
@@ -3,38 +3,62 @@ package datasources.sqs;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.*;
 import com.google.inject.Inject;
+import org.jetbrains.annotations.NotNull;
 import play.Configuration;
 
 import javax.inject.Named;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
 public class SQSClient {
+    private static final int SQS_MESSAGE_MAX_LENGTH = 256 * 1024;
     private final AmazonSQS sqs;
     private String queueUrl;
 
     @Inject
     public SQSClient(AmazonSQS sqs, @Named("sqs") Configuration sqsConf) {
         this.sqs = sqs;
-        Region region = RegionUtils.getRegion((sqsConf.getString("region", "eu-west-1"));
+        Region region = RegionUtils.getRegion(sqsConf.getString("region", "eu-west-1"));
         sqs.setRegion(region);
+    }
+
+    public boolean hasMessageBodyAValidLength(String messageBody) {
+        return messageBody.getBytes(Charset.defaultCharset()).length <= SQS_MESSAGE_MAX_LENGTH;
     }
 
     public void sendMessage(String messageBody) {
         sqs.sendMessage(new SendMessageRequest(queueUrl, messageBody));
     }
 
-    public CompletionStage<List<String>> receiveMessages() {
+    public CompletionStage<Boolean> receiveMessages(ProcessMessage processMessage) {
         return CompletableFuture.supplyAsync(() -> {
             ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
             List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
-            return messages.stream().map(Message::getBody).collect(Collectors.toList());
+            List<String> messagesBody = messages.stream().map(Message::getBody).collect(Collectors.toList());
+
+            boolean processed = processMessage.executed(messagesBody);
+
+            if (processed) {
+                List<DeleteMessageBatchRequestEntry> deleteMessageBatchRequestEntries =  messages.stream().map(Message::getReceiptHandle).map(this::getDeleteMessageBatchRequestEntry).collect(Collectors.toList());
+                sqs.deleteMessageBatch(new DeleteMessageBatchRequest(queueUrl, deleteMessageBatchRequestEntries));
+            }
+
+            return processed;
         });
     }
+
+    @NotNull
+    private DeleteMessageBatchRequestEntry getDeleteMessageBatchRequestEntry(String s) {
+        return new DeleteMessageBatchRequestEntry(UUID.randomUUID().toString(), s);
+    }
+}
+
+interface ProcessMessage {
+    boolean executed(List<String> messages);
 }
