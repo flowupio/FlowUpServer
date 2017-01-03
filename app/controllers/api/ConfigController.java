2be9e308c5abe4fd133efc5f33088db7242712de
Sergio Gutierrez <sergio@gokarumi.com>
2017-01-03 11:54:35 +0100

    Add a new async method to retrieve api key config

--- app/controllers/api/ConfigController.java
+++ app/controllers/api/ConfigController.java
@@ -1,27 +1,21 @@
 package controllers.api;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import controllers.api.ApiKeySecured;
-import controllers.api.HeaderParsers;
 import play.libs.Json;
 import play.mvc.Result;
 import play.mvc.Security;
-import sampling.SamplingGroup;
-import usecases.models.ApiKeyConfig;
 import usecases.GetApiKeyConfig;
+import usecases.models.ApiKeyConfig;
 
 import javax.inject.Inject;
+import java.util.concurrent.CompletionStage;
 
 import static play.mvc.Controller.request;
-import static play.mvc.Http.Status.PRECONDITION_FAILED;
 import static play.mvc.Results.ok;
-import static play.mvc.Results.status;
 
 @Security.Authenticated(ApiKeySecured.class)
 public class ConfigController {
 
     private static final String X_API_KEY = "X-Api-Key";
-
 
     private final GetApiKeyConfig getConfig;
 
@@ -30,13 +24,11 @@ public class ConfigController {
         this.getConfig = getConfig;
     }
 
-    public Result getConfig() {
-        ApiKeyConfig apiKeyConfig = getApiKeyConfig();
-        JsonNode content = Json.toJson(apiKeyConfig);
-        return ok(content);
+    public CompletionStage<Result> getConfig() {
+        return getApiKeyConfig().thenApply(apiKeyConfig -> ok(Json.toJson(apiKeyConfig)));
     }
 
-    private ApiKeyConfig getApiKeyConfig() {
+    private CompletionStage<ApiKeyConfig> getApiKeyConfig() {
         String apiKeyValue = request().getHeader(X_API_KEY);
         String uuid = request().getHeader(HeaderParsers.X_UUID);
         return getConfig.execute(apiKeyValue, uuid);
