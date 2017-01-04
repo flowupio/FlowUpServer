1602301689dfd26294b028ccfdbbf95d1fa52fe0
Sergio Gutierrez <sergio@gokarumi.com>
2017-01-04 13:13:05 +0100

    Replace lombok models by good-old models

--- app/usecases/models/Billing.java
+++ app/usecases/models/Billing.java
@@ -1,10 +1,7 @@
 package usecases.models;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class Billing {
     private final String fullName;
     private final String email;
@@ -17,4 +14,53 @@ public class Billing {
     private final String currentPlan;
 
     private final List<Transaction> transactions;
+
+    public Billing(String fullName, String email, String address, String postalCode, String city, String country,
+                   String cardNumber, String currentPlan, List<Transaction> transactions) {
+        this.fullName = fullName;
+        this.email = email;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.country = country;
+        this.cardNumber = cardNumber;
+        this.currentPlan = currentPlan;
+        this.transactions = transactions;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCurrentPlan() {
+        return currentPlan;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
 }
