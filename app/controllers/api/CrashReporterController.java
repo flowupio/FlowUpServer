1769e285cd57b49f05c1c1a77dcf1c0a4b3ac8c9
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-03-15 18:14:50 +0100

    Update ErrorReport data to include all the information comming from our headers

--- app/controllers/api/CrashReporterController.java
+++ app/controllers/api/CrashReporterController.java
@@ -10,6 +10,8 @@ import javax.inject.Inject;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import static controllers.api.HeaderParsers.X_API_KEY;
+
 @With(ApiKeySecuredAction.class)
 public class CrashReporterController extends Controller {
 
@@ -21,8 +23,11 @@ public class CrashReporterController extends Controller {
     }
 
     public CompletionStage<Result> reportClientError() {
+        Http.Request request = request();
         Http.RequestBody body = request().body();
         ErrorReport errorReport = Json.fromJson(body.asJson(), ErrorReport.class);
+        errorReport.setLibraryVersion(request.getHeader(USER_AGENT));
+        errorReport.setApiKey(request.getHeader(X_API_KEY));
         return CompletableFuture.supplyAsync(() -> {
             reportClientError.execute(errorReport);
             return created(Json.toJson(errorReport));
--- app/usecases/models/ErrorReport.java
+++ app/usecases/models/ErrorReport.java
@@ -1,5 +1,6 @@
 package usecases.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
@@ -10,6 +11,9 @@ public class ErrorReport {
     private final boolean batterySaverOn;
     private final String message;
     private final String stackTrace;
-
+    @JsonIgnore
+    private String libraryVersion;
+    @JsonIgnore
+    private String apiKey;
 
 }
