4bab2e620d3c19ec5290c535284f2f3eae0389be
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-26 12:06:38 +0200

    Minor refactor to simplify dashboard mapper

--- app/usecases/mapper/DashboardMapper.java
+++ app/usecases/mapper/DashboardMapper.java
@@ -20,17 +20,18 @@ public class DashboardMapper {
     }
 
     public JsonNode map(Application application, Dashboard dashboard) {
+        return Json.newObject()
+                .put("overwrite", true)
+                .put("orgId", application.getGrafanaOrgId())
+                .set("dashboard", mapDashboard(dashboard));
+    }
+
+    private JsonNode mapDashboard(Dashboard dashboard) {
         try {
-            return Json.newObject()
-                    .put("overwrite", true)
-                    .put("orgId", application.getGrafanaOrgId())
-                    .set("dashboard", mapper.readTree(dashboard.getJsonDescription()));
+            return mapper.readTree(dashboard.getJsonDescription());
         } catch (IOException e) {
             Logger.error("Failed mapping a dashboard to store it in Grafana", e);
-            return Json.newObject()
-                    .put("overwrite", true)
-                    .put("orgId", application.getGrafanaOrgId())
-                    .set("dashboard", Json.newObject());
+            return Json.newObject();
         }
     }
 }
