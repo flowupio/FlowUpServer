54e68d36c5dfa5d1907af5c177fdc6c2bd83fbc0
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-28 18:37:02 +0200

    Update script to repeat the upload process for every org in a range

--- scripts/update_dashboards.sh
+++ scripts/update_dashboards.sh
@@ -3,44 +3,46 @@
 GRAFANA_USER="admin"
 GRAFANA_PASSWORD="admin"
 
-USER_ID=""
-ORGANIZATION_ID="389"
 PLATFORM="ios"
 
-for DASHBOARD_FILE in resources/dashboards/$PLATFORM/*.json
+for ORGANIZATION_ID in {394..394}
 do
-    DASHBOARD=`cat $DASHBOARD_FILE`
-    printf "\n\nUploading dashboard $DASHBOARD_FILE...\n"
-    curl -vvv https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/dashboards/db\
-         -H "Accept: application/json"\
-         -H "Content-Type: application/json"\
-         --data "{\"overwrite\": true, \"orgId\": $ORGANIZATION_ID, \"dashboard\": $DASHBOARD}"
-done
-
-HOME_DASHBOARD_ID=`curl -s https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/search?limit=100\&query=\
-     -H "Accept: application/json"\
-     -H "Content-Type: application/json" | 
-     jq '.[] | {slug: .uri, id: .id} | if .slug == "db/home" then .id else 0 end' |
-     grep -v '0'`
-if [ ! -z "$HOME_DASHBOARD_ID" ] 
-then
-    printf "\nUpdating the home dashboard to $HOME_DASHBOARD_ID..."
-
-    USER_IDS=`curl -s https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/orgs/$ORGANIZATION_ID/users\
-        -H "Accept: application/json"\
-        -H "Content-Type: application/json" | 
-        jq '.[] | .userId'`
-    
-    for USER_ID in $USER_IDS
+    printf "\n[Migrating dashboards for organization with id $ORGANIZATION_ID]"
+    for DASHBOARD_FILE in resources/dashboards/$PLATFORM/*.json
     do
-        echo "USER $USER_ID"
+        DASHBOARD=`cat $DASHBOARD_FILE`
+        printf "\nUploading dashboard $DASHBOARD_FILE...\n"
+        curl -s https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/dashboards/db\
+            -H "Accept: application/json"\
+            -H "Content-Type: application/json"\
+            --data "{\"overwrite\": true, \"orgId\": $ORGANIZATION_ID, \"dashboard\": $DASHBOARD}"
     done
 
-    # curl -vvv https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/user/preferences\
-    #      -X PUT\
-    #      -H "Accept: application/json"\
-    #      -H "Content-Type: application/json"\
-    #      --data "{\"userId\": $USER_ID, \"orgId\": $ORGANIZATION_ID, \"homeDashboardId\": $HOME_DASHBOARD_ID}"
-else
-    printf "\nNo home dashboard found"
-fi
+    HOME_DASHBOARD_ID=`curl -s https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/search?limit=100\&query=\&orgId=$ORGANIZATION_ID\
+        -H "Accept: application/json"\
+        -H "Content-Type: application/json" | 
+        jq '.[] | {slug: .uri, id: .id} | if .slug == "db/home" then .id else 0 end' |
+        grep -v '^0$'`
+    if [ ! -z "$HOME_DASHBOARD_ID" ] 
+    then
+        printf "\nUpdating home dashboard to the one with id $HOME_DASHBOARD_ID..."
+
+        USER_IDS=`curl -s https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/orgs/$ORGANIZATION_ID/users\
+            -H "Accept: application/json"\
+            -H "Content-Type: application/json" | 
+            jq '.[] | .userId'`
+        
+        for USER_ID in $USER_IDS
+        do
+            printf "\n\t> to user $USER_ID"
+            curl -s https://$GRAFANA_USER:$GRAFANA_PASSWORD@dashboards.flowup.io/api/user/preferences\
+                -X PUT\
+                -H "Accept: application/json"\
+                -H "Content-Type: application/json"\
+                --data "{\"userId\": $USER_ID, \"orgId\": $ORGANIZATION_ID, \"homeDashboardId\": $HOME_DASHBOARD_ID}"
+        done
+    else
+        printf "\nNo home dashboard found"
+    fi
+
+done
