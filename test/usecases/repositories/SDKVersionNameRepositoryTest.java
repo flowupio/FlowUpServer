5c59996a887b92d4e6a15a2b134356e48344d89b
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-29 18:53:40 +0200

    Add tests

--- test/usecases/repositories/SDKVersionNameRepositoryTest.java
+++ test/usecases/repositories/SDKVersionNameRepositoryTest.java
@@ -18,6 +18,7 @@ import static org.junit.Assert.assertEquals;
 public class SDKVersionNameRepositoryTest extends WithFlowUpApplication implements WithResources {
 
     public static final String MAVEN_CENTRAL_SEARCH_QUERY = "/solrsearch/select?q=g%3A%22io.flowup%22%20AND%20a%3A%22android-sdk%22";
+    public static final String COCOAPODS_SEARCH_QUERY = "/api/pods?query=FlowUpIOSSDK";
     @Rule
     public WireMockRule wireMockRule = new WireMockRule();
 
@@ -46,7 +47,7 @@ public class SDKVersionNameRepositoryTest extends WithFlowUpApplication implemen
     }
 
     @Test
-    public void returnsTheDefaultVersionIfTheResponseIsAnError() throws Exception {
+    public void returnsTheDefaultVersionIfTheMavenCentralResponseIsAnError() throws Exception {
         stubFor(get(urlEqualTo(MAVEN_CENTRAL_SEARCH_QUERY))
                 .willReturn(aResponse()
                         .withStatus(404)));
@@ -57,7 +58,7 @@ public class SDKVersionNameRepositoryTest extends WithFlowUpApplication implemen
     }
 
     @Test
-    public void returnsTheDefaultVersionIfTheSearchResponseIsEmpty() throws Exception {
+    public void returnsTheDefaultVersionIfTheMavenCentralSearchResponseIsEmpty() throws Exception {
         stubFor(get(urlEqualTo(MAVEN_CENTRAL_SEARCH_QUERY))
                 .willReturn(aResponse()
                         .withBody(getFile("mavencentral/mavenCentralSearchArtifactEmptyResponse.json"))
@@ -68,4 +69,38 @@ public class SDKVersionNameRepositoryTest extends WithFlowUpApplication implemen
         assertEquals("<LATEST_VERSION>", sdkVersion);
     }
 
+    @Test
+    public void parsesAndReturnsTheIOSSDKArtifactVersion() throws Exception {
+        stubFor(get(urlEqualTo(COCOAPODS_SEARCH_QUERY))
+                .willReturn(aResponse()
+                        .withBody(getFile("cocoapods/cocoapodsSearchArtifactResponse.json"))
+                        .withStatus(200)));
+
+        String sdkVersion = repository.getLatestSDKVersionName(Platform.IOS).toCompletableFuture().get();
+
+        assertEquals("2.0.0", sdkVersion);
+    }
+
+    @Test
+    public void returnsTheDefaultVersionIfTheCocoapodsResponseIsAnError() throws Exception {
+        stubFor(get(urlEqualTo(COCOAPODS_SEARCH_QUERY))
+                .willReturn(aResponse()
+                        .withStatus(404)));
+
+        String sdkVersion = repository.getLatestSDKVersionName(Platform.IOS).toCompletableFuture().get();
+
+        assertEquals("<LATEST_VERSION>", sdkVersion);
+    }
+
+    @Test
+    public void returnsTheDefaultVersionIfTheCocoapodsSearchResponseIsEmpty() throws Exception {
+        stubFor(get(urlEqualTo(COCOAPODS_SEARCH_QUERY))
+                .willReturn(aResponse()
+                        .withBody(getFile("cocoapods/cocoapodsSearchArtifactEmptyResponse.json"))
+                        .withStatus(200)));
+
+        String sdkVersion = repository.getLatestSDKVersionName(Platform.IOS).toCompletableFuture().get();
+
+        assertEquals("<LATEST_VERSION>", sdkVersion);
+    }
 }
